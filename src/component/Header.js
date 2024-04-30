d6c069700ddcac4541de213c21ea0d84fca7fbb3
pawan <thakunipawan@gmail.com>
2024-04-30 14:35:35 +0530

    commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Project from './Project'
 
 const Header = () => {
    
@@ -7,18 +8,31 @@ const Header = () => {
     <div>
     
     
-    <img  className="ml-[750px] rounded-full mt-20 border border-green-800 w-20" alt="logo" src="https://lh3.googleusercontent.com/ogw/AF2bZyjzCeh-fJvxvKaCgkPUI6Sk16Kf6ds2r_fRRobFNRTBpg=s32-c-mo"></img>
+    <img  className="ml-[160px] md:ml-[720px] rounded-full mt-20 border border-green-800 w-20" alt="logo" src="https://lh3.googleusercontent.com/ogw/AF2bZyjzCeh-fJvxvKaCgkPUI6Sk16Kf6ds2r_fRRobFNRTBpg=s32-c-mo"></img>
    
-    <ul className='m-2'>
-      <li className='text-bold p-3 ml-6 h-20 '>Pawan Thakuni</li>
-      <li className='ml-10 mt-1'>Frontend Developer</li>
+    <ul className='m-2 shadow-lg'>
+      <li className='font-bold p-3  my-0 text-xl'>Pawan Thakuni</li>
+      <li className=' my-0 font-serif '>Frontend Developer</li>
       
-    
+    <div className='flex justify-center my-6'>
      
       <a href="https://www.linkedin.com/in/pawan-thakuni-390a2324b/?originalSubdomain=in" target="_blank" rel="noopener noreferrer"> 
-      <img  className="h-7 w-7 ml-96" alt='lidin' src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/108px-LinkedIn_icon.svg.png"/>Visit LinkedIn</a>
-     
+      <img  className="h-7 w-7 mr-8" alt='lidin' src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/108px-LinkedIn_icon.svg.png"/></a>
+  
+      <a href="https://github.com/pawan-thakuni-au45" target="_blank" rel="noopener noreferrer"> 
+      <img  className="h-7 w-7 " alt='lidin' src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"/></a>
+
+
+         
+      <a href="mailto:thakunipawan@gmail.com" target="_blank" rel="noopener noreferrer"> 
+      <img  className="h-7 w-7 ml-7" alt='lidin' src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/112-gmail_email_mail-512.png"/></a>
+
+      
+      <a href="tel:+91 8279831158" target="_blank" rel="noopener noreferrer"> 
+      <img  className="h-7 w-7 ml-7" alt='lidin' src="https://cdn-icons-png.freepik.com/256/2354/2354127.png?uid=R145302671&semt=ais_hybrid"/></a>
+      </div>
   </ul>
+        <Project/>
 
    
     </div>
---
+++ src/component/Project.js
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    
+     <div className=''>
+     <h1 className='font-bold'>Projects </h1>
+     <div className='flex  flex-col md:flex-row my-6 justify-between '>
+       <div className='flex flex-col ml-[8px] shadow-2xl'>
+       
+        <h1 className="font-bold">Netflix-GPT</h1>
+
+       
+          <a className="text-blue-600  " href="https://fun-gpt.netlify.app/" target="_blank" rel="noopener noreferrer">Live Link</a>
+          <a className="text-blue-600  " href="https://github.com/pawan-thakuni-au45" target="_blank" rel="noopener noreferrer">GitHub Link</a>
+
+          <li className='text-red-900'>A netflix clone powered by GPT</li>
+          <ul className='font-bold'>Features</ul>
+            <li>Netflix-inspired UI with dynamic movie categories. </li>
+            <li>Detailed movie information from TMDB API.</li>
+            <li>Personalized movie recommendations powered by GPT. </li>
+            <li>eamless navigation with React and React Router.</li>
+            <li>Authentication using Firebase. </li>
+            <li>Stylish design using Tailwind CSS.</li>
+            
+          
+          <ul className='font-bold'>Tech Stack</ul>
+             <li>React | React Router | Tailwind CSS | Node.js | Firebase | GPT API | TMDB API</li>
+          
+         </div>
+
+         <div className='flex flex-col ml-[8px] shadow-2xl'>
+       
+         <h1 className="font-bold">YouTube-Clone</h1>
+ 
+        
+           <a className="text-blue-600" href="https://fun-gpt.netlify.app/" target="_blank" rel="noopener noreferrer">Live Link</a>
+          <a className="text-blue-600  " href="https://github.com/pawan-thakuni-au45" target="_blank" rel="noopener noreferrer">GitHub Link</a>
+
+          
+           <ul className='font-bold'>Features</ul>
+           
+             <li>Optimized Video Searching using Debouncing Technique . </li>
+             <li>Flexible Sidebar.</li>
+             <li>FLive chat box which shows dummy chats and also gives ability to input messages including Dummy comments and nested comments display. </li>
+             <li>Using YouTube's video suggestions API for fetching videos list.</li>
+             <li>Using Google's live keyword suggestion API and caching the results with redux if the user makes same Keyword search for the same session. </li>
+             <li>Stylish design using Tailwind CSS.</li>
+             
+             
+           <ul className='font-bold'>Tech Stack</ul>
+              <li>React|React Router Dom | React Redux |Redux Toolkit |Tailwind CSS | Vite for bundling</li>
+          
+          </div>
+
+          <div className='flex flex-col ml-[8px] shadow-2xl'>
+         <h1 className="font-bold">FoodGrab</h1>
+ 
+        
+         <a className='text-blue-600' href="https://fun-gpt.netlify.app/" target="_blank" rel="noopener noreferrer">Live Link</a>
+         <a className="text-blue-600  " href="https://github.com/pawan-thakuni-au45" target="_blank" rel="noopener noreferrer">GitHub Link</a>
+
+        
+         <ul className='font-bold'>Features</ul>
+         <li>A dynamic food delivery app leveraging Swiggy's Live API</li>
+           <li>Real-Time Menu Updates. </li>
+           <li>Super Fast Loading with Lazy Loading and Chunking.</li>
+           <li>Smart Search & Filters for easy discovery. </li>
+           <li> User-Friendly Navigation.</li>
+           <li>Fully Responsive Design. </li>
+           <li>Stylish design using Tailwind CSS.</li>
+           
+       
+         <ul className='font-bold'>Tech Stack</ul>
+            <li>React | React Router | Tailwind CSS | Node.js | Firebase | GPT API | TMDB API</li>
+        
+         </div>
+
+          </div>
+    </div>
+  )
+}
+
+export default Project
